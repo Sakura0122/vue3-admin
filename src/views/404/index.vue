63b7bf5a46b689b1d8d503e6e5e52ea4ab363a41
sakura <11854578@qq.com>
2023-09-21 08:57:10 +0800

    feat: 404静态页面搭建

---
+++ src/views/404/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="error-page">
+    <svg-icon name="404" width="400px" height="400px" />
+    <router-link to="/" style="margin-top: 50px">
+      <el-button type="primary">回到首页</el-button>
+    </router-link>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.error-page {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
